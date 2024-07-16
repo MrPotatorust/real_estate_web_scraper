380950286a17d76c1eb5be494d04e9d06f62b330
MrPotatorust <bumko96@gmail.com>
2024-07-16 23:29:16 +0200

    Scraping nearly finished

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
+import pandas as pd
+import numpy as np
+
 import time
+
+
+def convert_to_num(num):
+    return num.replace(",", ".").replace(" ", "")
+
+
+
+
 
 
 driver = webdriver.Chrome()
@@ -13,6 +24,7 @@ wait = WebDriverWait(driver, 10)
 #by changing the number you can choose if you want to rent or buy
 rent_or_buy = 1
 type_of_property = 1
+data = {"price": [], "price_per_sq_m": [], "sq_meter": [], "location": [], "type_of_property": []}
 
 
 
@@ -57,8 +69,6 @@ wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.css-1nte1ih"))).
 #waits for the page to load
 wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.advertisement-item--content__price")))
 
-prices = driver.find_elements(By.CSS_SELECTOR, "div.advertisement-item--content__price")
-
 num_pages = driver.find_element(By.XPATH, "(//li[contains(@class, 'component-pagination__item')])[5]")
 
 
@@ -66,8 +76,46 @@ num_pages = driver.find_element(By.XPATH, "(//li[contains(@class, 'component-pag
 url = driver.current_url
 
 
-for i in range(2, int(num_pages.text)):
-    driver.get((url+f"?p[page]={i}"))
-    time.sleep(2)
+
+for page in range(2, int(num_pages.text)):
+    #getting the data
+    prices = driver.find_elements(By.CSS_SELECTOR, "div.advertisement-item--content__price")
+    main_info = driver.find_elements(By.CSS_SELECTOR, "div.advertisement-item--content__info")
+
+    for price in prices:
+        text = price.text
+        index = text.find("€")
+
+        if index == -1:
+            data["price"].append(np.nan)
+            data["price_per_sq_m"].append(np.nan)
+        else:
+            data["price"].append(int(convert_to_num(text[:index])))
+            data["price_per_sq_m"].append(float(convert_to_num(text[index+2:-4])))
+
+
+    count = 0
+    for info in main_info:
+        count += 1
+
+        if count == 1:
+            adress = info.text
+        if count == 2:
+            sq_meter_index = info.text.find("•")
+            property_type = info.text[:sq_meter_index-1]
+            sq_meters = float(convert_to_num(info.text[sq_meter_index+2:-3]))
+            print(adress) 
+            print(property_type)
+            print(sq_meters)
+            print("=======================")
+            count = 0
+
+
+
+    driver.get(url+f"?p[page]={page}")
+    time.sleep(200)
 
 time.sleep(500)
+
+
+driver.quit()
